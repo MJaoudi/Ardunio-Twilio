169ad21eb1fedcf0d0ccbfe7f6bc5452ef9bb67f
Mike Jaoudi <mjaoudi93@gmail.com>
2013-05-01 17:52:59 -0400

    Added Account SID

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 # Download the Python helper library from twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
-auth_token=open("APIKEY").read().strip()
+
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC32a3c49700934481addd5ce1659f04d2"
+account_sid = open("APISID").read().strip()
+auth_token=open("APIKEY").read().strip()
 client = TwilioRestClient(account_sid, auth_token)
 
-print auth_token
- 
+print account_sid
+
 message = client.sms.messages.create(body="Jenny please?! I love you <3",
     to="+14159352345",
     from_="+14158141829")
